f0393acf43862b313ec0509c78b18bcf46d3eac2
krishnadhoundiyal <krisnand2k@gmail.com>
2022-01-13 15:29:12 +0530

    Profile pic

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
             <div className="three columns">
               <img
                 className="profile-pic"
-                src="../images/profile.png"
+                src="../images/profile.jpg"
                 alt="Radhe Pic"
               />
             </div>
